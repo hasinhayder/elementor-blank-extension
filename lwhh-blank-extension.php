3475c987b90d39ea5fccd940806a03655d15e5bb
Hasin Hayder <countdraculla@gmail.com>
2019-08-31 14:31:55 +0600

    Boilerplate Blank Extension

---
+++ lwhh-blank-extension.php
@@ -0,0 +1,239 @@
+<?php
+/*
+Plugin Name: LWHH Blank Extension
+Plugin URI:
+Description: Boilerplate for creating Elementor Extensions
+Version: 1.0
+Author: Hasin
+Author URI: https://hasin.me
+License: GPLv2 or later
+Text Domain: ebe
+Domain Path: /languages/
+*/
+
+final class Elementor_Blank_Extension {
+
+	/**
+	 * Plugin Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string The plugin version.
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access private
+	 * @static
+	 *
+	 * @var Elementor_Test_Extension The single instance of the class.
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 * @static
+	 *
+	 * @return Elementor_Test_Extension An instance of the class.
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function __construct() {
+
+		add_action( 'init', [ $this, 'i18n' ] );
+		add_action( 'plugins_loaded', [ $this, 'init' ] );
+
+	}
+
+	/**
+	 * Load Textdomain
+	 *
+	 * Load plugin localization files.
+	 *
+	 * Fired by `init` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function i18n() {
+
+		load_plugin_textdomain( 'ebe' );
+
+	}
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * Load the plugin only after Elementor (and other plugins) are loaded.
+	 * Checks for basic plugin requirements, if one check fail don't continue,
+	 * if all check have passed load the files required to run the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init() {
+
+		// Check if Elementor installed and activated
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+			return;
+		}
+
+		// Check for required Elementor version
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
+			return;
+		}
+
+		// Check for required PHP version
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
+			return;
+		}
+
+		// Add Plugin actions
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have Elementor installed or activated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_missing_main_plugin() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'ebe' ),
+			'<strong>' . esc_html__( 'Elementor Blank Extension', 'ebe' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'ebe' ) . '</strong>'
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required Elementor version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_elementor_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'ebe' ),
+			'<strong>' . esc_html__( 'Elementor Blank Extension', 'ebe' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'ebe' ) . '</strong>',
+			self::MINIMUM_ELEMENTOR_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_php_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'ebe' ),
+			'<strong>' . esc_html__( 'Elementor Blank Extension', 'ebe' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'ebe' ) . '</strong>',
+			self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Init Widgets
+	 *
+	 * Include widgets files and register them
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init_widgets() {
+
+		// Include Widget files
+		require_once( __DIR__ . '/widgets/lwhh-blank-widget.php' );
+
+		// Register widget
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \LWHH_Blank_Widget() );
+
+	}
+
+}
+
+Elementor_Blank_Extension::instance();
---
+++ widgets/lwhh-blank-widget.php
@@ -0,0 +1,165 @@
+<?php
+
+
+class LWHH_Blank_Widget extends \Elementor\Widget_Base {
+
+	/**
+	 * Get widget name.
+	 *
+	 * Retrieve Blank widget name.
+	 *
+	 * @return string Widget name.
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 */
+	public function get_name() {
+		return 'blank_widget';
+	}
+
+	/**
+	 * Get widget title.
+	 *
+	 * Retrieve Blank widget title.
+	 *
+	 * @return string Widget title.
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 */
+	public function get_title() {
+		return __( 'Blank Widget', 'ebe' );
+	}
+
+	/**
+	 * Get widget icon.
+	 *
+	 * Retrieve Blank widget icon.
+	 *
+	 * @return string Widget icon.
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 */
+	public function get_icon() {
+		return 'fa fa-code';
+	}
+
+	/**
+	 * Get widget categories.
+	 *
+	 * Retrieve the list of categories the Blank widget belongs to.
+	 *
+	 * @return array Widget categories.
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 */
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	/**
+	 * Register Blank widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function _register_controls() {
+
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => __( 'Content', 'ebe' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'dummy_text',
+			[
+				'label'       => __( 'Dummy Text', 'ebe' ),
+				'type'        => \Elementor\Controls_Manager::TEXT,
+				'input_type'  => 'text',
+				'placeholder' => __( 'Some Dummy Text', 'ebe' ),
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->register_style_controls();
+
+	}
+
+	/**
+	 * Register Blank widget style ontrols.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function register_style_controls() {
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => __( 'Common', 'ebe' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'color',
+			[
+				'label'      => __( 'Color', 'ebe' ),
+				'type'       => \Elementor\Controls_Manager::COLOR,
+				'default'    => '#ff0000',
+				'selectors' => [
+					'{{WRAPPER}} .dummy_text' => 'color: {{VALUE}}'
+				]
+			]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	/**
+	 * Render Blank widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function render() {
+
+		$settings = $this->get_settings_for_display(); //and echo $settings['dummy_text']
+		$dummy_text= $this->get_settings('dummy_text');
+		$this->add_render_attribute('dummy_text','class','dummy_text')
+		?>
+		<div <?php echo $this->get_render_attribute_string('dummy_text') ?>> <?php echo esc_html($dummy_text); ?></div>
+		<?php
+
+
+	}
+
+	/**
+	 * Render Blank widget output on the frontend.
+	 *
+	 * Written in JS and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function _content_template() {
+		$this->add_render_attribute('dummy_text','class','dummy_text')
+		?>
+			<div <?php echo $this->get_render_attribute_string('dummy_text') ?>> {{ settings.dummy_text }}</div>
+		<?php
+	}
+
+}
